87747a0d57b811648c650bd52e5c63e186661ed7
Miguel Ángel <txrz56rad@live.com.mx>
2025-05-28 23:01:20 -0600

    enhance CtaComponent for reusability with dynamic inputs

--- src/app/cta/cta.component.ts
+++ src/app/cta/cta.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-cta',
-  imports: [ButtonComponent],
+  standalone: true,
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './cta.component.html',
   styleUrl: './cta.component.css',
 })
-export class CtaComponent {}
+export class CtaComponent {
+  @Input() headline: string = 'BACK IN STOCK: Discover our rarest opal';
+  @Input() subheadline: string =
+    'Herzegovinian Opal - A singular gemstone marvel';
+  @Input() description: string =
+    'If you missed out last year, now’s your chance to experience something truly unique - these rare Herzegovinian opals, small as a pearl, shimmer with hues of azure, violet, and gold. A must-have for those seeking timeless luxury and distinction.';
+  @Input() buttonText: string = 'Shop Now';
+  @Input() buttonStyle: 'primary' | 'secondary' | 'outline' = 'secondary';
+  @Input() bgColor: string = 'bg-seashell';
+  @Input() reverseLayout: boolean = false;
+  @Input() paddingY: boolean = false;
+  @Input() textPaddingLefth: boolean = true;
+  @Input() images: { src: string; alt: string }[] = [
+    { src: '/cta.jpg', alt: 'CTA image' },
+  ];
+}
