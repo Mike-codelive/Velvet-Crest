63051df2c97a5f8dca838aedbee608d78b63ba8b
Miguel Ángel <txrz56rad@live.com.mx>
2025-05-31 04:42:22 -0600

    implement dynamic-height collection slider with Swiper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, AfterViewInit } from '@angular/core';
 import { HeroSliderComponent } from './hero-slider/hero-slider.component';
 import { FeaturedComponent } from './featured/featured.component';
 import { CtaComponent } from './cta/cta.component';
+import { CollectionSliderComponent } from './collection-slider/collection-slider.component';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,7 +10,12 @@ import { CtaComponent } from './cta/cta.component';
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeroSliderComponent, FeaturedComponent, CtaComponent],
+  imports: [
+    HeroSliderComponent,
+    FeaturedComponent,
+    CtaComponent,
+    CollectionSliderComponent,
+  ],
 })
 export class AppComponent implements AfterViewInit {
   isLoaded = false;
---
+++ src/app/collection-slider/collection-slider.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import Swiper from 'swiper';
+
+Swiper.use([]);
+
+@Component({
+  selector: 'app-collection-slider',
+  imports: [],
+  templateUrl: './collection-slider.component.html',
+  styleUrl: './collection-slider.component.css',
+})
+export class CollectionSliderComponent {
+  slides = [
+    {
+      image: '/collection_1.jpg',
+      title: 'Mexico City',
+      id: 0,
+      description: 'A radiant gem from Mexico City, crafted with precision.',
+    },
+    {
+      image: '/collection_2.jpg',
+      title: 'Albanian',
+      id: 1,
+      description:
+        'A luminous opal with ethereal beauty, exclusive to VelvetCrest.',
+    },
+    {
+      image: '/collection_3.jpg',
+      title: 'Bosnia',
+      id: 2,
+      description: 'Rare opal from Bosnia, showcasing timeless elegance.',
+    },
+    {
+      image: '/collection_4.jpg',
+      title: 'Croatia',
+      id: 3,
+      description: 'Rare opal from Bosnia, showcasing timeless elegance.',
+    },
+    {
+      image: '/collection_5.jpg',
+      title: 'Romania',
+      id: 4,
+      description: 'Rare opal from Bosnia, showcasing timeless elegance.',
+    },
+    {
+      image: '/collection_6.jpg',
+      title: 'Germany',
+      id: 5,
+      description: 'Rare opal from Bosnia, showcasing timeless elegance.',
+    },
+  ];
+
+  swiper!: Swiper;
+
+  ngAfterViewInit() {
+    this.swiper = new Swiper('.collection-swiper', {
+      modules: [],
+      // autoHeight: true,
+      slidesPerView: 1,
+      spaceBetween: 8,
+      breakpoints: {
+        850: { slidesPerView: 2.2 },
+        1025: { slidesPerView: 3.2 },
+        1200: { slidesPerView: 4.2 },
+      },
+      // pagination: {
+      //   el: '.swiper-pagination-hero',
+      //   clickable: true,
+      // },
+    });
+  }
+}
